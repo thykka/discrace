b6b2434e9b2e56a3e4c9c0121d49eac35d3dabf3
Moses Holmström <thykka@gmail.com>
2022-02-18 01:54:45 +0200

    rules, turns, moving

--- modules/game.js
+++ modules/game.js
@@ -1,4 +1,4 @@
-import Tiles from './game-tiles.js';
+import Tiles, { TileStart } from './game-tiles.js';
 import Levels from './game-levels.js';
 import PlayerColors from './player-colors.js';
 export class RacingGame {
@@ -12,6 +12,186 @@ export class RacingGame {
     };
     Object.assign(this, defaults, options);
     this.levels = this.loadLevels(Levels);
+  }
+
+  command(commandName, { user, args }) {
+    const command = this.commands[commandName];
+    if(typeof command !== 'function') return;
+    return command.call(this, { user, args });
+  }
+  
+  handleNewMatch({ args, user }) {
+    // check if match is already running (on this channel?)
+    if(this.matchState) {
+      // check if player already joined
+      const playerAlreadyJoined = this.matchState.players.some(
+        player => player.id === user.id
+      );
+      if(playerAlreadyJoined) {
+        return {
+          success: false, reaction: '🚫',
+          reply: `you've already joined`
+        };
+      }
+      // check if existing players have already moved
+      const playersHaveMoved = this.matchState.players.some(
+        player => player.path.length > 1
+      );
+      console.log(this.matchState.players);
+      if(playersHaveMoved) {
+        return {
+          success: false, reaction: '🚫',
+          reply: `race already started`
+        };
+      }
+
+      // add player to match
+      this.matchState.players.push(
+        this.createPlayer(user, this.matchState.players.length)
+      );
+      this.assignSpawns();
+      return {
+        success: true, reaction: '🏁',
+        messageText: `${ user.name } joined the race!`,
+        matchState: this.matchState
+      };
+    }
+    
+    //TODO: Allow loading player maps
+    // Let players load levels by name
+    const level = this.levels[args];
+
+    this.matchState = this.createMatch(user, level);
+    this.assignSpawns();
+
+    return {
+      success: true, reaction: '🏁',
+      messageText: `${ user.name } started a new race!`,
+      matchState: this.matchState
+    };
+  }
+
+  createMatch(user, level = Object.values(this.levels)[0]) {
+    const players = [
+      this.createPlayer(user, 0)
+    ];
+    const matchState = { level, players, turn: 0 };
+    return matchState;
+  }
+
+  createPlayer(user, index = 0) {
+    return {
+      ...user,
+      color: PlayerColors[index % PlayerColors.length] || '#F0F',
+      index,
+      path: [],
+      x: 0, y: 0,
+      dx: 0, dy: 0
+    };
+  }
+
+  assignSpawns(state = this.matchState) {
+    const spawnCells = state.level.filter(cell => cell.spawn);
+    if(!spawnCells.length) throw Error('No way hombre');
+    state.players.forEach((player, index) => {
+      const cell = spawnCells[index % spawnCells.length];
+      player.x = cell.x;
+      player.y = cell.y;
+      player.path = [[cell.x, cell.y]];
+    });
+  }
+
+  // TODO: allow move number without prefix
+  handleMovePlayer({ args, user }) {
+    // ensure there is an existing match
+    if(!this.matchState) {
+      return {
+        success: false,
+        //reaction: '🛑',
+        reply: 'no ongoing race'
+      };
+    }
+    // ensure player has joined the match
+    const playerIndex = this.matchState.players.findIndex(
+      ({id}) => id === user.id
+    );
+    if(playerIndex < 0) {
+      return {
+        success: false,
+        //reaction: '',
+        reply: 'you have not joined this race'
+      };
+    }
+    // player should only move on their own turn
+    if(this.matchState.turn !== playerIndex) {
+      return {
+        success: false,
+        //reaction: '',
+        reply: 'it\'s not your turn yet'
+      };
+    }
+    // arg should be a number
+    const player = this.matchState.players[playerIndex];
+    const moveIndex = parseInt(args) - 1;
+    if(isNaN(moveIndex) || moveIndex < 0 || moveIndex > 8) {
+      return {
+        success: false,
+        //reaction: '',
+        reply: 'use !move 1-9 to move, example: `!move 6`'
+      };
+    }
+    // if no moves available, the player loses
+    // TODO: remove player, check if players remain, end match...
+    const availableMoves = this.getMoves(player);
+    if(availableMoves.length === 0) {
+      this.movePlayer();
+      return {
+        success: true,
+        matchState: this.matchState,
+        reaction: '☠'
+      };
+    }
+    // arg should be within allowed moves
+    const move = availableMoves.find(move => move.index === moveIndex);
+    if(!move) {
+      return {
+        success: false,
+        //reaction: '',
+        reply: 'allowed moves: ' + availableMoves.map((_, i) => i).join(', ')
+      };
+    }
+
+    return this.movePlayer(move);
+  }
+
+  getMoves(player) {
+    // TODO: return a list of cells the player may move into
+    return [
+      { x: -1, y: -1 }, { x: 0, y: -1 }, { x: 1, y: -1 },
+      { x: -1, y:  0 }, { x: 0, y:  0 }, { x: 1, y:  0 },
+      { x: -1, y:  1 }, { x: 0, y:  1 }, { x: 1, y:  1 },
+    ]
+      .filter(move => true) // TODO: should not hit walls
+      .map(
+        (move, index) => ({ ...move, index })
+      );
+  }
+
+  movePlayer(
+    move,
+    player = this.matchState?.players[this.matchState?.turn]
+  ) {
+    player.dx += move.x;
+    player.dy += move.y;
+    player.x += player.dx;
+    player.y += player.dy;
+    // increment turn
+    this.matchState.turn =
+      (this.matchState.turn + 1) % this.matchState.players.length;
+    return {
+      success: true,
+      matchState: this.matchState
+    };
   }
 
   loadLevels(maps) {
@@ -40,68 +220,6 @@ export class RacingGame {
     });
   }
 
-  command(commandName, { username, userId, args }) {
-    const command = this.commands[commandName];
-    if(typeof command !== 'function') return;
-    return command.call(this, {
-      username,
-      userId,
-      args
-    });
-  }
-
-  handleNewMatch({ args, username, userId }) {
-    // check if match is already running (on this channel?)
-    if(this.matchState) {
-      return {
-        success: false,
-        reaction: '🚫'
-      };
-    }
-    
-    // Let players load levels by name
-    const level = this.levels[args];
-    //TODO: Allow loading player maps
-
-    const players = [
-      this.createPlayer({ userId, username }, level)
-    ];
-    return {
-      success: true,
-      reaction: '🏁',
-      matchState: this.createMatch(level, players)
-    };
-  }
-
-  createMatch(
-    level = Object.values(this.levels)[0],
-    players = []
-  ) {
-    this.matchState = { level, players };
-    this.assignSpawns(this.matchState);
-    return this.matchState;
-  }
-
-  createPlayer({ userId, username, index }, level) {
-    const color = PlayerColors[index || 0];
-    return { userId, username, color };
-  }
-
-  assignSpawns(state) {
-    const spawnCells = state.level.filter(cell => cell.spawn);
-    if(!spawnCells.length) throw Error('No way hombre');
-    state.players.forEach((player, index) => {
-      const cell = spawnCells[index % spawnCells.length];
-      player.x = cell.x;
-      player.y = cell.y;
-    });
-  }
-
-  handleMovePlayer({ username, userId, args }) {
-    return {
-      success: true,
-    }
-  }
 }
 
 export default RacingGame;
